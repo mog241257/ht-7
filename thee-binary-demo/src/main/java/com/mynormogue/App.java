916f4728d8f009e01d07b3a18858d4f8ca749719
mog241257 <mog241257@uvg.edu.gt>
2025-03-26 16:29:53 -0600

    se agrego la estructura de la hoja

---
+++ thee-binary-demo/src/main/java/com/mynormogue/App.java
@@ -0,0 +1,13 @@
+package com.mynormogue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
